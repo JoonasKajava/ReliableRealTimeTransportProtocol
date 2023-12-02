db986d9467ab39191f8d70ac094a828d854f8878
Joonas Kajava <JoonasKajava@users.noreply.github.com>
2023-12-02 17:08:25 +0200

    Rename server to sender and client to receiver, update buffer handling
    Renamed the "server" module to "sender" and "client" module to "receiver" for clearer understanding. Updated buffer handling in the transport protocol to sync buffer sizes and improved data fragmentation management. This also includes corresponding changes in .idea files and run configurations.

--- lib_rrttp/src/window.rs
+++ lib_rrttp/src/window.rs
@@ -21,6 +21,8 @@ pub struct Window {
 
     /// The highest sequence number that has been received.
     highest_received: Arc<Mutex<u32>>,
+
+    read_buffer: Arc<Mutex<Vec<u8>>>,
 }
 
 impl Window {
@@ -32,7 +34,8 @@ impl Window {
             window_size: WINDOW_SIZE as u32,
             socket: Arc::new(socket),
             highest_acknowledged: Arc::new(Mutex::new(0)),
-            highest_received: Arc::new(Mutex::new(0))
+            highest_received: Arc::new(Mutex::new(0)),
+            read_buffer: Arc::new(Mutex::new(Vec::new())),
         })
     }
 
@@ -40,6 +43,7 @@ impl Window {
         let socket = Arc::clone(&self.socket);
         let highest_acknowledged = Arc::clone(&self.highest_acknowledged);
         let highest_received = Arc::clone(&self.highest_received);
+        let read_buffer = Arc::clone(&self.read_buffer);
         thread::spawn(move || {
             loop {
                 let (_, buffer, _) = match socket.receive() {
@@ -71,14 +75,33 @@ impl Window {
                 {
                     let mut highest_received_guard = highest_received.lock().unwrap();
                     if sequence_number > *highest_received_guard {
-                        info!("Received frame with sequence number {}", sequence_number);
                         *highest_received_guard = sequence_number;
                     }
                 }
+
+                if let Some(options) = frame.get_options() {
+                    for option in options {
+                        match option.kind {
+                            OptionKind::BufferSize => {
+                                Window::sync_read_buffer(Arc::clone(&read_buffer), &option);
+                            }
+                        }
+                    }
+                }
+
                 let data = frame.get_data();
+
+
                 info!("Received frame with sequence number {} data: {}", sequence_number, from_utf8(data).unwrap());
             }
         })
+    }
+
+    fn sync_read_buffer(read_buffer: Arc<Mutex<Vec<u8>>>, option: &FrameOption) {
+        let buffer_size = u32::from_be_bytes(option.data.try_into().expect("Failed to convert buffer size to u32"));
+        let mut read_buffer_guard = read_buffer.lock().unwrap();
+        info!("Resizing read buffer to {}", buffer_size);
+        read_buffer_guard.resize(buffer_size as usize, 0);
     }
 
     pub fn send_ack(socket: &Socket, sequence_number: u32) -> std::io::Result<usize> {
@@ -90,7 +113,7 @@ impl Window {
     }
 
     pub fn send(&mut self, data_buffer: &[u8]) -> std::io::Result<usize> {
-        let data_size = data_buffer.len();
+        let data_size = data_buffer.len() as u32;
         let segments = data_size as f32 / MAX_DATA_SIZE as f32;
         let segments = segments.ceil() as u32;
 
@@ -120,7 +143,7 @@ impl Window {
 
             let buffer_shift = (sequence_number - 1) * MAX_DATA_SIZE as u32;
 
-            let buffer_left = data_size as u32 - buffer_shift;
+            let buffer_left = data_size - buffer_shift;
 
             let data_lower_bound = buffer_shift as usize;
             let data_upper_bound = (buffer_shift + min(buffer_left, MAX_DATA_SIZE as u32)) as usize;
