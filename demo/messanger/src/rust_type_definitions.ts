ecb2dc2bc11868518389c195ae72959eb8b28e98
Joonas Kajava <JoonasKajava@users.noreply.github.com>
2024-02-25 00:42:05 +0200

    Adding new event types to rust_type_definitions.ts
    Two new event types, "FileDataReceived" and "InvalidFileResponse", have been added to the rust_type_definitions.ts file. This change enhances the functionality and error handling by introducing a separate event for file data reception and invalid file responses.

--- demo/messanger/src/rust_type_definitions.ts
+++ demo/messanger/src/rust_type_definitions.ts
@@ -3,28 +3,30 @@
 */
 
 export interface NetworkFileInfo {
-	file_name: string;
-	file_mime?: string;
-	file_size_in_bytes: number;
+    file_name: string;
+    file_mime?: string;
+    file_size_in_bytes: number;
 }
 
-export type LogSuccessMessage = 
-	| { type: "UnknownMessage", content: string }
-	| { type: "MessageReceived", content: string }
-	| { type: "MessageSent", content: string }
-	| { type: "LocalSocketBindSuccess", content: string }
-	| { type: "ConnectedToRemote", content: string }
-	| { type: "FileInfoSent", content: NetworkFileInfo }
-	| { type: "FileInfoReceived", content: NetworkFileInfo }
-	| { type: "ReceivedAcknowledgement", content?: undefined }
-	| { type: "FileRejected", content?: undefined }
-	| { type: "FileAccepted", content?: undefined }
-	| { type: "FileResponseSent", content?: undefined };
+export type LogSuccessMessage =
+    | { type: "UnknownMessage", content: string }
+    | { type: "MessageReceived", content: string }
+    | { type: "MessageSent", content: string }
+    | { type: "LocalSocketBindSuccess", content: string }
+    | { type: "ConnectedToRemote", content: string }
+    | { type: "FileInfoSent", content: NetworkFileInfo }
+    | { type: "FileInfoReceived", content: NetworkFileInfo }
+    | { type: "FileDataReceived", content?: undefined }
+    | { type: "ReceivedAcknowledgement", content?: undefined }
+    | { type: "FileRejected", content?: undefined }
+    | { type: "FileAccepted", content?: undefined }
+    | { type: "FileResponseSent", content?: undefined };
 
-export type LogErrorMessage = 
-	| { type: "MessageSendError", content: string }
-	| { type: "LocalSocketBindFailed", content: string }
-	| { type: "LocalSocketNotBound", content?: undefined }
-	| { type: "ConnectionError", content: string }
-	| { type: "FileSendError", content: string };
+export type LogErrorMessage =
+    | { type: "MessageSendError", content: string }
+    | { type: "LocalSocketBindFailed", content: string }
+    | { type: "LocalSocketNotBound", content?: undefined }
+    | { type: "ConnectionError", content: string }
+    | { type: "FileSendError", content: string }
+    | { type: "InvalidFileResponse", content: string };
 
